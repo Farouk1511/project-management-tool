61d782dc460b8818e2bf29782ff4a9e93b4f91ac
Farouk <kazeem.farouk@yahoo.com>
2024-07-05 12:10:50 -0400

    fixed issues with members apollo client

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,18 +1,19 @@
 import gql from "graphql-tag";
-import { getClient } from "../lib/client";
+//import { getClient } from "../lib/client";
 
 export default async function Members() {
-  const client = getClient();
+  // const client = getClient();
 
-  const query = gql`
-    query ExampleQuery {
-      hello
-    }
-  `;
+  // const query = gql`
+  //   query ExampleQuery {
+  //     hello
+  //   }
+  // `;
 
-  const {data} = await client.query({query})
-  console.log(data)
+  // const {data} = await client.query({query})
+  // console.log(data)
 
 
-  return <div>This is the members page {data.hello} </div>;
+  return <div>This is the members page </div>;
+  //return <div>This is the members page {data.hello} </div>;
 }
